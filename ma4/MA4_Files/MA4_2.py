21ea460262a97489a075bad7b86b69d6a108be0e
Rpetters <148333677+Rpetters@users.noreply.github.com>
2023-10-18 13:57:46 +0200

    Update MA4_2.py

--- ma4/MA4_Files/MA4_2.py
+++ ma4/MA4_Files/MA4_2.py
@@ -6,14 +6,14 @@ from matplotlib import pyplot as plt
 
 
 
-# Pure Python function
+#Python function
 def fib_py(n):
     if n <= 1:
         return n
     else:
         return(fib_py(n-1) + fib_py(n-2))
 
-# Numba-accelerated function
+#Numba function
 @njit
 def fib_numba(n):
     if n <= 1:
@@ -22,33 +22,33 @@ def fib_numba(n):
         return(fib_numba(n-1) + fib_numba(n-2))
 
 def time_functions_and_plot():
-    n_values = list(range(20, 46))
+    n_values = list(range(10, 20))
     times_py = []
     times_numba = []
     times_cpp = []
 
     # Time the functions
     for n in n_values:
-        # Python
+        #Python
         start = time.perf_counter()
         fib_py(n)
         end = time.perf_counter()
         times_py.append(end - start)
 
-        # Numba
+        #Numba
         start = time.perf_counter()
         fib_numba(n)
         end = time.perf_counter()
         times_numba.append(end - start)
 
-        # C++
+        #C++
         person = Person(n)
         start = time.perf_counter()
         person.fib()
         end = time.perf_counter()
         times_cpp.append(end - start)
 
-    # Create plots
+    #Create plots
     plt.figure()
     plt.plot(n_values, times_py, label='Python')
     plt.plot(n_values, times_numba, label='Numba')
@@ -59,7 +59,7 @@ def time_functions_and_plot():
     plt.savefig('fibonacci_timings.png')  # Save figure
 
     # Calculate Fibonacci number for n = 47 using C++ and Numba
-    n = 47
+    n = 10
     person = Person(n)
     print("Fibonacci for n=47 (C++):", person.fib())
     print("Fibonacci for n=47 (Numba):", fib_numba(n))
